73e7aff9ace407768152eeaf0b30e9292877a3a7
Ramon181 <ramonantonio29970476@gmail.com>
2023-05-18 10:31:58 -0500

    Configuaracion de laravel con la base de datos

---
+++ app/Models/Producto.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    public function tienda()
+    {
+        return $this->belongsTo(Tienda::class);
+    }
+}
---
+++ resources/views/Tienda/create.blade.php
@@ -0,0 +1,11 @@
+creacion de tiendas
+
+<form class="d-flex" action="" method="post" enctype="multipart/form-data"" >
+<label for="nombre">Nombre</label>
+<input type="text" name="nombre" id="nombre">
+<br>
+<label for="fecha_apertura">Fecha Apertura</label>
+<input type="date" name="fecha_apertura" id="fecha_apertura">
+<br>
+<input type="submit" value="Enviar">
+</form>
